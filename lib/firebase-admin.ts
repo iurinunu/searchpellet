7059e93fba1214db3519f0e3cea523f1be6c2934
iurinunu <iurinunu@gmail.com>
2022-07-27 20:40:55 +0100

    private_key

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
     }),
     databaseURL: "https://fast-feedback-demo-123-default-rtdb.firebaseio.com"
